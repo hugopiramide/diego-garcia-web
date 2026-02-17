329ce0fea0818b27cdcc5f47c3de39755492eb52
hugopiramide <hugocastillo.deus@gmail.com>
2026-02-17 20:50:16 +0100

    Add scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
+      <ScrollToTop />
       <Header />
       <main className="flex-1 flex flex-col">
         <Outlet />
